07ac958c642cd78f2e6f49a3e73a606ba3697901
Jaio <jaio0701@gmail.com>
2024-03-02 23:32:05 -0500

    feat: reset scores/usernames button

--- dashboard/js/player-management.js
+++ dashboard/js/player-management.js
@@ -8,6 +8,7 @@ const p2ScoreRep = nodecg.Replicant('p2Score', {defaultValue: 0});
 const p3ScoreRep = nodecg.Replicant('p3Score', {defaultValue: 0});
 const p4ScoreRep = nodecg.Replicant('p4Score', {defaultValue: 0});
 
+// Input/output elements
 const p1NameInput = document.getElementById('p1-name');
 const p2NameInput = document.getElementById('p2-name');
 const p3NameInput = document.getElementById('p3-name');
@@ -17,6 +18,7 @@ const p2ScoreOutput = document.getElementById('p2-score');
 const p3ScoreOutput = document.getElementById('p3-score');
 const p4ScoreOutput = document.getElementById('p4-score');
 
+// Update player names based on Enter key
 p1NameInput.addEventListener('keydown', (e) => {
     if(e.key === 'Enter')
     {
@@ -66,6 +68,7 @@ function incPoints(player, value)
     }
 }
 
+// BS stuff to get the dashboard to update properly
 p1NameRep.on('change', (newValue) => {
     p1NameInput.value = newValue;
     console.log(`Player 1 name updated to ${newValue}`);
@@ -98,3 +101,22 @@ p4ScoreRep.on('change', (newValue) => {
     p4ScoreOutput.innerHTML = `Current score: ${p4ScoreRep.value}`;
     console.log(`Player 4 score updated to ${newValue}`);
 });
+
+// Reset all
+function reset()
+{
+    if (confirm('Are you sure you want to reset all scores and usernames?'))
+    {
+        p1NameRep.value = '';
+        p2NameRep.value = '';
+        p3NameRep.value = '';
+        p4NameRep.value = '';
+        p1ScoreRep.value = 0;
+        p2ScoreRep.value = 0;
+        p3ScoreRep.value = 0;
+        p4ScoreRep.value = 0;
+        console.log('All scores and usernames reset.');
+    } else {
+        console.log('Reset cancelled.');
+    }
+}
